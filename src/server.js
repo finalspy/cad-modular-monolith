12e169bc45c91adb64e176a31d5bf9d39ed9f3c3
ypetit <public@ypetit.net>
2025-05-27 14:37:00 +0200

    fix: update tests config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- vitest.config.js
+++ vitest.config.js
@@ -3,11 +3,23 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    include: ['tests/**/*.test.js'],
+    environment: "node",
+    include: ["tests/**/*.test.js"],
     coverage: {
-      reporter: ['text', 'html'],
-      exclude: ['src/app.js'],
+      reporter: ["text", "html"],
+      exclude: [
+        "src/server.js",
+        "src/app.js",
+        "*.config.js",
+        "**/*.config.js",
+        "cypress/**",
+        "public/**",
+        "**/*.cy.js",
+        "**/*.cy.ts",
+      ],
     },
+  },
+  server: {
+    port: 4000,
   },
 });
